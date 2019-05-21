00d33981672cce0d97164a20db8c2b474283c357
SimarpalKalsi <Kepler186f>
2019-05-21 17:08:59 -0400

    .
    .

--- src/main/java/serialcomms/ListnerHandler.java
+++ src/main/java/serialcomms/ListnerHandler.java
@@ -4,10 +4,14 @@ import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
 
+import java.util.ArrayList;
+
 /**
  * The type Listner handler.
  */
 public class ListnerHandler {
+
+    public static
 
     /**
      * Attach reader listner.
@@ -26,10 +30,13 @@ public class ListnerHandler {
             public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_RECEIVED; }
             public void serialEvent(SerialPortEvent event)
             {
+                String line = "";
                 byte[] newData = event.getReceivedData();
                 System.out.println("Received data of size: " + newData.length);
-                for (int i = 0; i < newData.length; ++i)
-                    System.out.print((char)newData[i]);
+                for (int i = 0; i < newData.length; ++i) {
+                    System.out.print((char) newData[i]);
+                }
+                String[] lines = line.split("\\r?\\n");
                 System.out.println("\n");
             }
         });
