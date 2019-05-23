93293110c00adeba7c7d2527c8fea50a633da834
Ritesh <riteshparmar17@gmail.com>
2019-05-23 12:18:40 -0400

    added printer dao & querry

--- src/main/java/pojo/printer/Printer.java
+++ src/main/java/pojo/printer/Printer.java
@@ -9,6 +9,11 @@ import serialcomms.SerialCommunicator;
 public class Printer {
 
     /**
+     * The Id.
+     */
+    int id;
+
+    /**
      * The Serial communicator.
      */
     SerialCommunicator serialCommunicator;
@@ -84,10 +89,11 @@ public class Printer {
      * @param homeOffset           the home offset
      * @param controlLoopPID       the control loop pid
      * @param isHomeCentre         the is home centre
+     * @param id                   the id
      */
     public Printer(SerialCommunicator serialCommunicator, String printerName, String firmware, String printerDesc, BuildVolume buildVolume,
                    StepsPerUnit stepsPerUnit, MaxFeedrate maxFeedrate, AccelerationSettings accelerationSettings, AdditionalInfo additionalInfo, HomeOffset homeOffset,
-                   ControlLoopPID controlLoopPID, boolean isHomeCentre) {
+                   ControlLoopPID controlLoopPID, boolean isHomeCentre, int id) {
         this.serialCommunicator = serialCommunicator;
         this.printerName = printerName;
         this.firmware = firmware;
@@ -100,6 +106,7 @@ public class Printer {
         this.homeOffset = homeOffset;
         this.controlLoopPID = controlLoopPID;
         this.isHomeCentre = isHomeCentre;
+        this.id= id;
     }
 
     /**
@@ -318,6 +325,23 @@ public class Printer {
         isHomeCentre = homeCentre;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+//String controlLoopPidJson = controlLoopPID.jsonSerializable(controlLoopPID);
 
 }
